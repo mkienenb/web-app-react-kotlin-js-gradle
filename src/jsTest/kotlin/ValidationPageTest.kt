ba3287f8bf1d5decb190f5a3db6eb97c77fef3a1
Mike Kienenberger <mkienenb@gmail.com>
2025-04-16 11:01:57 -0400

    test: restore and fix ValidationPageTest.kt

--- src/jsTest/kotlin/ValidationPageTest.kt
+++ src/jsTest/kotlin/ValidationPageTest.kt
@@ -1,36 +1,36 @@
-//import kotlin.test.*
-//import kotlinx.coroutines.test.runTest
-//import react.create
-//import react.dom.client.createRoot
-//import react.dom.test.act
-//import web.dom.document
-//import web.html.HTMLDivElement
-//import web.html.HTMLInputElement
-//
-//
-//class ValidationPageTest {
-//
-//    private lateinit var container: HTMLDivElement
-//
-//    @BeforeTest
-//    fun setup() {
-//        container = document.createElement("div") as HTMLDivElement
-//        document.body.appendChild(container)
-//    }
-//
-//    @AfterTest
-//    fun teardown() {
-//        container.remove()
-//    }
-//
-//    @Test
-//    fun shouldRenderValidationForm() = runTest {
-//        act {
-//            val root = createRoot(container)
-//            root.render(ValidationPage.create())
-//        }
-//
-//        val input = container.querySelector("[data-test='iban-entry']") as? HTMLInputElement
-//        assertNotNull(input, "Expected IBAN input field to be present")
-//    }
-//}
+import kotlin.test.*
+import kotlinx.coroutines.test.runTest
+import react.create
+import react.dom.client.createRoot
+import react.dom.test.act
+import web.dom.document
+import web.html.HTMLDivElement
+import web.html.HTMLInputElement
+
+
+class ValidationPageTest {
+
+    private lateinit var container: HTMLDivElement
+
+    @BeforeTest
+    fun setup() {
+        container = document.createElement("div") as HTMLDivElement
+        document.body.appendChild(container)
+    }
+
+    @AfterTest
+    fun teardown() {
+        container.remove()
+    }
+
+    @Test
+    fun shouldRenderValidationForm() = runTest {
+        act {
+            val root = createRoot(container)
+            root.render(ValidationPage.create())
+        }
+
+        val input = container.querySelector("[data-test='iban-entry']") as? HTMLInputElement
+        assertNotNull(input, "Expected IBAN input field to be present")
+    }
+}
