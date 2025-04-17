7693481de145b5771e48991fd2e21ae2c28e10f8
Mike Kienenberger <mkienenb@gmail.com>
2025-04-17 14:57:51 -0400

    test: finish iShouldSeeTheFollowingListOfUnwatchedVideos() step impementation

--- src/jvmTest/kotlin/cucumber/confexplorer/ViewVideoPage.kt
+++ src/jvmTest/kotlin/cucumber/confexplorer/ViewVideoPage.kt
@@ -1,15 +1,19 @@
-package com.example.validateiban
+package cucumber.confexplorer
 
 import cucumber.common.page.BasePage
 import cucumber.common.page.GenerateCucumberPageHelper
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
 import org.openqa.selenium.support.PageFactory
 
 @GenerateCucumberPageHelper
 class ViewVideoPage(driver: WebDriver) : BasePage(driver) {
-    @FindBy(css = "")
-    private lateinit var unwatchedVideoNameList: List<String>
+
+    // TODO: revisit if @FindBy works if element is found
+    val unwatchedVideoNameList: List<String>
+        get() = driver.findElements(By.cssSelector("li[data-code-element-handle='unwatchedVideo']")).map { it.text }
 
     init {
         PageFactory.initElements(driver, this)
