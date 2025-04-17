f70dd745869611204f886ca01d4661e257b550d0
Mike Kienenberger <mkienenb@gmail.com>
2025-04-17 10:12:49 -0400

    test: switch to using scenarioContext.withIbanPage {}

--- src/jvmTest/kotlin/cucumber/validateiban/IbanPage.kt
+++ src/jvmTest/kotlin/cucumber/validateiban/IbanPage.kt
@@ -1,11 +1,13 @@
 package cucumber.validateiban
 
 import cucumber.common.page.BasePage
+import cucumber.common.page.GenerateCucumberPageHelper
 import org.openqa.selenium.Keys
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
 
+@GenerateCucumberPageHelper
 class IbanPage(driver: WebDriver) : BasePage(driver) {
     @FindBy(css = "form [data-test='iban-entry']")
     private lateinit var ibanEntryField: WebElement
