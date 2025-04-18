ddbe3d61d03330bfa11c63846f533ac75713d64d
Mike Kienenberger <mkienenb@gmail.com>
2025-04-18 15:36:50 -0400

    chore: make linux react tools generic

--- tools/kill-react-app-processes.sh
+++ tools/kill-react-app-processes.sh
@@ -1,6 +1,6 @@
 for pid in $(pgrep java); do
   cwd=$(pwdx $pid 2>/dev/null | awk '{print $2}')
-  if [ "$cwd" = "/home/mkienenb/IdeaProjects/gvea/web-app-react-kotlin-js-gradle" ]; then
+    if [[ $cwd == */web-app-react-kotlin-js-gradle ]]; then
     echo $pid
   fi
 done | xargs -r kill
