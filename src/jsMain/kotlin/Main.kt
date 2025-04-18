237289582615d4db2633d32a72ae859ecee95100
Mike Kienenberger <mkienenb@gmail.com>
2025-04-18 15:08:58 -0400

    feat: hardcode expected unwatched list

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -2,6 +2,8 @@ import react.FC
 import react.Props
 import react.create
 import react.dom.client.createRoot
+import react.dom.html.ReactHTML.li
+import testsupport.dataCodeElementHandleAttribute
 import web.dom.document
 
 fun main() {
@@ -12,4 +14,20 @@ fun main() {
 }
 
 val root = FC<Props> {
+    li {
+        dataCodeElementHandleAttribute = "unwatchedVideo"
+        +"Learning kotlin"
+    }
+    li {
+        dataCodeElementHandleAttribute = "unwatchedVideo"
+        +"Learning koin"
+    }
+    li {
+        dataCodeElementHandleAttribute = "unwatchedVideo"
+        +"Learning kotest"
+    }
+    li {
+        dataCodeElementHandleAttribute = "unwatchedVideo"
+        +"Learning react"
+    }
 }
