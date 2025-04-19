3034f49703b05886999ab7864bc1fe914b0f8ca0
Mike Kienenberger <mkienenb@gmail.com>
2025-04-19 11:06:21 -0400

    feat: finished tutorial

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,5 +1,17 @@
 import kotlinx.browser.document
+import kotlinx.serialization.Serializable
+import react.create
+import react.dom.client.createRoot
 
 fun main() {
-    document.bgColor = "red"
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+    createRoot(container).render(App.create())
 }
+
+@Serializable
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
---
+++ src/jsMain/kotlin/VideoList.kt
@@ -0,0 +1,24 @@
+import react.FC
+import react.Props
+import react.dom.html.ReactHTML.p
+
+external interface VideoListProps : Props {
+    var videos: List<Video>
+    var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
+}
+
+val VideoList = FC<VideoListProps> { props ->
+    for (video in props.videos) {
+        p {
+            key = video.id.toString()
+            onClick = {
+                props.onSelectVideo(video)
+            }
+            if (video == props.selectedVideo) {
+                +"▶ "
+            }
+            +"${video.speaker}: ${video.title}"
+        }
+    }
+}
