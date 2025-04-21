b27a201322777efe61d70f9aa2b876a13d577ba0
tmv <tmv@gvea.com>
2025-04-21 10:52:06 -0800

    feat: add learning kotlin li

--- src/jsMain/kotlin/confexplorer/viewvideo/VideoListReactComponent.kt
+++ src/jsMain/kotlin/confexplorer/viewvideo/VideoListReactComponent.kt
@@ -3,6 +3,7 @@ package confexplorer.viewvideo
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.ul
+import react.dom.html.ReactHTML.li
 
 external interface VideoListProps : Props {
     var videos: List<Video>
@@ -10,5 +11,8 @@ external interface VideoListProps : Props {
 
 val VideoListReactComponent = FC<VideoListProps> {
     ul {
+        li {
+            +"Learning kotlin"
+        }
     }
 }
