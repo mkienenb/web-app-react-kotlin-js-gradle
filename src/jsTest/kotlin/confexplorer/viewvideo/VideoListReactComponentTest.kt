c4473aa39a9fab6ce75acd0ba3b31609ba53a93e
jzdewilde <jzdewilde@gvea.com>
2025-04-21 11:01:47 -0800

    test(refactor): update "show Learning kotlin video" to query for text of element

--- src/jsTest/kotlin/confexplorer/viewvideo/VideoListReactComponentTest.kt
+++ src/jsTest/kotlin/confexplorer/viewvideo/VideoListReactComponentTest.kt
@@ -3,10 +3,7 @@ package confexplorer.viewvideo
 import browserOnlyCode
 import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import kotest.ReactComponentTestBase
-import web.html.HTMLLIElement
-import web.html.HTMLUListElement
 
 class VideoListReactComponentTest : ReactComponentTestBase() {
     init {
@@ -15,10 +12,9 @@ class VideoListReactComponentTest : ReactComponentTestBase() {
                 ForComponent(VideoListReactComponent, {
                     videos = listOf(Video(1, "Learning kotlin"))
                 }) {
-                    val unorderedListElement = container.getElementsByTagName("ul")[0] as? HTMLUListElement
-                    val firstVideoListItem = unorderedListElement?.getElementsByTagName("li")?.get(0) as? HTMLLIElement
+                    val firstVideoTitle = container.querySelector("ul li")?.textContent
                     withClue("unordered list") {
-                        firstVideoListItem?.innerText shouldBe "Learning kotlin"
+                        firstVideoTitle shouldBe "Learning kotlin"
                     }
                 }
             }
