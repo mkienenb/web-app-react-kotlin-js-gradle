9172799c7a47574f158206d4cd513fe24742d3f0
RootCellar <RootCellar9877@gmail.com>
2025-04-21 11:58:53 -0800

    feat: add header to app component with Conference Explorer title

--- src/jsMain/kotlin/confexplorer/AppComponent.kt
+++ src/jsMain/kotlin/confexplorer/AppComponent.kt
@@ -2,7 +2,11 @@ package confexplorer
 
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.h1
 
 
 val AppComponent = FC<Props> {
+    h1 {
+        +"Conference Explorer"
+    }
 }
