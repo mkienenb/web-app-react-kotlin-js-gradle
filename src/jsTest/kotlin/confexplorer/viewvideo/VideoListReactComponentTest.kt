7fe60044e33f9ac5af64a13066c0ab809be257ad
RootCellar <RootCellar9877@gmail.com>
2025-04-21 10:48:26 -0800

    test: should show Learning kotlin video

--- src/jsTest/kotlin/confexplorer/viewvideo/VideoListReactComponentTest.kt
+++ src/jsTest/kotlin/confexplorer/viewvideo/VideoListReactComponentTest.kt
@@ -2,25 +2,23 @@ package confexplorer.viewvideo
 
 import browserOnlyCode
 import io.kotest.assertions.withClue
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import kotest.ReactComponentTestBase
-import kotlinx.coroutines.test.runTest
-import react.Props
-import react.create
-import react.dom.client.createRoot
-import react.dom.test.act
+import web.html.HTMLLIElement
 import web.html.HTMLUListElement
 
 class VideoListReactComponentTest : ReactComponentTestBase() {
     init {
         browserOnlyCode {
-            should("render video list") {
+            should("show Learning kotlin video") {
                 ForComponent(VideoListReactComponent, {
                     videos = listOf(Video(1, "Learning kotlin"))
                 }) {
-                    val unorderedList = container.getElementsByTagName("ul")[0] as? HTMLUListElement
+                    val unorderedListElement = container.getElementsByTagName("ul")[0] as? HTMLUListElement
+                    val firstVideoListItem = unorderedListElement?.getElementsByTagName("li")?.get(0) as? HTMLLIElement
                     withClue("unordered list") {
-                        unorderedList shouldNotBe null
+                        firstVideoListItem?.innerText shouldBe "Learning kotlin"
                     }
                 }
             }
