fa75c8ba55b17997989c38b277f15cb4c6958194
tmv <tmv@gvea.com>
2025-04-22 10:26:24 -0800

    feat: write video list to app FC

--- src/jsMain/kotlin/confexplorer/AppComponent.kt
+++ src/jsMain/kotlin/confexplorer/AppComponent.kt
@@ -1,12 +1,21 @@
 package confexplorer
 
+import api.VideoService
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.p
+import testsupport.dataCodeElementHandleAttribute
 
 
 val AppComponent = FC<Props> {
     h1 {
         +"Conference Explorer"
+    }
+    VideoService.getVideos().forEach { video ->
+        p {
+            dataCodeElementHandleAttribute="unwatchedVideo"
+            +video.title
+        }
     }
 }
