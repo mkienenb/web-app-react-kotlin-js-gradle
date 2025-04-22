8abeeee6aeacd16952aea8a1795795480539e242
RootCellar <RootCellar9877@gmail.com>
2025-04-22 11:49:33 -0800

    test: should show unwatched video titles of 'Unlearning Java' on page 2

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -4,6 +4,7 @@ import confexplorer.viewvideo.Video
 
 object VideoService {
     private var fetchFunction : (Int) -> Video? = {null}
+    private var fetchURLFunction : (String) -> Video? = {null}
 
     fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
@@ -11,6 +12,10 @@ object VideoService {
 
     fun setFetchFunction(fetchFunction: (Int) -> Video?) {
         this.fetchFunction = fetchFunction
+    }
+
+    fun setFetchURLFunction(fetchURLFunction: (String) -> Video?) {
+        this.fetchURLFunction = fetchURLFunction
     }
 
     fun getVideo(videoId: Int): Video? {
