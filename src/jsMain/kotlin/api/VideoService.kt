26d8a288fa5b36211459288fcc82dd1e2a755b1c
jzdewilde <jzdewilde@gvea.com>
2025-04-22 12:05:45 -0800

    test: update "show unwatched video titles of 'Learning Kotlin' and 'Unlearning Java' on page" to work with Json

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -4,6 +4,7 @@ import confexplorer.viewvideo.Video
 
 object VideoService {
     private var fetchURLFunction : (String) -> Video? = {null}
+    private var fetchURLToJsonFunction : (String) -> String? = {null}
 
     fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
@@ -11,6 +12,10 @@ object VideoService {
 
     fun setFetchURLFunction(fetchURLFunction: (String) -> Video?) {
         this.fetchURLFunction = fetchURLFunction
+    }
+
+    fun setFetchURLToJsonFunction(fetchURLToJsonFunction: (String) -> String?) {
+        this.fetchURLToJsonFunction = fetchURLToJsonFunction
     }
 
     fun getVideo(videoId: Int): Video? {
