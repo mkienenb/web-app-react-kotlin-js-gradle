ffdcc184b84d47175e00d675ee423ff804c53cb8
lavalo98 <lavaloprogramming@gmail.com>
2025-04-22 09:58:49 -0800

    test: show unwatched video titles list on page

--- src/jsTest/kotlin/confexplorer/AppComponentTest.kt
+++ src/jsTest/kotlin/confexplorer/AppComponentTest.kt
@@ -2,7 +2,9 @@ package confexplorer
 
 import browserOnlyCode
 import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
+import js.array.asList
 import kotest.ReactComponentTestBase
 
 class AppComponentTest: ReactComponentTestBase() {
@@ -13,6 +15,17 @@ class AppComponentTest: ReactComponentTestBase() {
                     val pageHeader = container.querySelector("h1")?.textContent
                     withClue("page header") {
                         pageHeader shouldBe "Conference Explorer"
+                    }
+                }
+            }
+
+            should("show unwatched video titles list on page") {
+                ForComponent(AppComponent) {
+                    val actualUnwatchedVideoTitlesList = container.querySelectorAll("[data-code-element-handle='unwatchedVideo']")
+                        .asList()
+                        .map { it.textContent }
+                    withClue("unwatched video titles") {
+                        actualUnwatchedVideoTitlesList shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
                     }
                 }
             }
