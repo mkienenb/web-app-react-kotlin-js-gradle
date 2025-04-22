1f220259b53dc6fe2a8dd67b05ae73e36b540a2f
lavalo98 <lavaloprogramming@gmail.com>
2025-04-22 11:11:18 -0800

    test: using setFetchFunction() instead of setVideo()

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -4,11 +4,17 @@ import confexplorer.viewvideo.Video
 
 object VideoService {
     private var videos = emptyList<Video>()
+    private var fetchFunction : (Int) -> Video? = {null}
+
     fun getVideos(): List<Video> {
         return videos
     }
 
     fun setVideos(videos: List<Video>) {
         this.videos = videos
+    }
+
+    fun setFetchFunction(fetchFunction: (Int) -> Video?) {
+        this.fetchFunction = fetchFunction
     }
 }
