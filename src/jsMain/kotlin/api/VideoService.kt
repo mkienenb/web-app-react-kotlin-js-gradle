edb90f7ea5faabead0a1c056cc475198af583e03
RootCellar <RootCellar9877@gmail.com>
2025-04-22 11:28:44 -0800

    refactor: delete dead code

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -3,15 +3,10 @@ package api
 import confexplorer.viewvideo.Video
 
 object VideoService {
-    private var videos = emptyList<Video>()
     private var fetchFunction : (Int) -> Video? = {null}
 
     fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
-    }
-
-    fun setVideos(videos: List<Video>) {
-        this.videos = videos
     }
 
     fun setFetchFunction(fetchFunction: (Int) -> Video?) {
