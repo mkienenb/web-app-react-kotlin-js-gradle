3c9c0cba345514b92c30a1d79c6dabac4f64bcb5
tmv <tmv@gvea.com>
2025-04-23 10:30:50 -0800

    test: extract createFetchFunction and fix it

--- src/jsTest/kotlin/api/VideoServiceTest.kt
+++ src/jsTest/kotlin/api/VideoServiceTest.kt
@@ -10,12 +10,10 @@ import kotlinx.serialization.json.Json
 class VideoServiceTest : ShouldSpec({
 
     should("show unwatched video titles of 'Learning Kotlin' and 'Unlearning Java' on page") {
-        VideoService.setFetchURLToJsonFunction { url ->
-            Json.encodeToString(
-                listOf(
-                    Video(1, "Learning Kotlin"), Video(2, "Unlearning Java")
-                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
-        }
+        val videoList = listOf(
+            Video(1, "Learning Kotlin"), Video(2, "Unlearning Java")
+        )
+        VideoService.setFetchURLToJsonFunction(createFetchFunction(videoList))
         val actualVideoTitleList = VideoService.getVideos().map { it.title }
         withClue("unwatched video titles") {
             actualVideoTitleList shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
@@ -23,12 +21,8 @@ class VideoServiceTest : ShouldSpec({
     }
 
     should("show unwatched video titles of 'Learning Kotlin' on page") {
-        VideoService.setFetchURLToJsonFunction { url ->
-            Json.encodeToString(
-                listOf(
-                    Video(1, "Learning Kotlin")
-                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
-        }
+        val videoList = listOf(Video(1, "Learning Kotlin"))
+        VideoService.setFetchURLToJsonFunction(createFetchFunction(videoList))
         val actualUnwatchedVideoTitlesList = VideoService.getVideos().map { it.title }
         withClue("unwatched video titles") {
             actualUnwatchedVideoTitlesList shouldContainExactly listOf("Learning Kotlin")
@@ -36,15 +30,25 @@ class VideoServiceTest : ShouldSpec({
     }
 
     should("show unwatched video titles of 'Unlearning Java' on page 2") {
-        VideoService.setFetchURLToJsonFunction { url ->
-            Json.encodeToString(
-                listOf(
-                    Video(2, "Unlearning Java")
-                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
-        }
+        val videoList = listOf(Video(2, "Unlearning Java"))
+        VideoService.setFetchURLToJsonFunction(createFetchFunction(videoList))
         val actualUnwatchedVideoTitlesList = VideoService.getVideos().map { it.title }
         withClue("unwatched video titles") {
             actualUnwatchedVideoTitlesList shouldContainExactly listOf("Unlearning Java")
         }
     }
 })
+
+private fun createFetchFunction(videoList: List<Video>): (String) -> String {
+    return {
+        url ->
+            val video = videoList.firstOrNull {
+                it.id == url.substringAfterLast('/').toIntOrNull()
+            }
+            if (video == null) {
+                "{}"
+            } else {
+                Json.encodeToString(video)
+            }
+    }
+}
