6abc340de3651d5ac6cc193ceff60a67f86dda30
jzdewilde <jzdewilde@gvea.com>
2025-04-23 10:49:28 -0800

    test: add "fetch video title Responses of 'Learning Kotlin'"

--- src/jsTest/kotlin/api/VideoServiceTest.kt
+++ src/jsTest/kotlin/api/VideoServiceTest.kt
@@ -6,6 +6,10 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.collections.shouldContainExactly
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.w3c.fetch.Response
+import org.w3c.fetch.ResponseInit
+import org.w3c.files.Blob
+import org.w3c.files.BlobPropertyBag
 
 class VideoServiceTest : ShouldSpec({
 
@@ -37,6 +41,16 @@ class VideoServiceTest : ShouldSpec({
             actualUnwatchedVideoTitlesList shouldContainExactly listOf("Unlearning Java")
         }
     }
+
+    should("fetch video title Responses of 'Learning Kotlin'") {
+        val videoList = listOf(Video(1, "Learning Kotlin"))
+        VideoService.setFetchURLToResponseFunction(createResponseFetchFunction(videoList))
+        val actualUnwatchedVideoTitlesList = VideoService.getVideos().map { it.title }
+        withClue("unwatched video titles") {
+            actualUnwatchedVideoTitlesList shouldContainExactly listOf("Learning Kotlin")
+        }
+    }
+
 })
 
 private fun createJsonFetchFunction(videoList: List<Video>): (String) -> String {
@@ -50,5 +64,22 @@ private fun createJsonFetchFunction(videoList: List<Video>): (String) -> String 
             } else {
                 Json.encodeToString(video)
             }
+    }
+}
+
+
+private fun createResponseFetchFunction(videoList: List<Video>): (String) -> Response {
+    return {
+            url ->
+        val video = videoList.firstOrNull {
+            it.id == url.substringAfterLast('/').toIntOrNull()
+        }
+        var json = "{}"
+        if (video != null) {
+            json = Json.encodeToString(video)
+        }
+        val blob = Blob(arrayOf(json), BlobPropertyBag(type = "application/json"))
+        val responseInit = ResponseInit(status = if (video != null) 200 else 404)
+        Response(blob, responseInit)
     }
 }
