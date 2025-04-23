76698a52e9c00db2415c6da8f085314b762332b2
tmv <tmv@gvea.com>
2025-04-23 09:51:11 -0800

    test: convert to node tests

--- src/jsTest/kotlin/api/VideoServiceTest.kt
+++ src/jsTest/kotlin/api/VideoServiceTest.kt
@@ -7,15 +7,14 @@ import io.kotest.matchers.collections.shouldContainExactly
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-class VideoServiceTest: ShouldSpec( {
+class VideoServiceTest : ShouldSpec({
 
     should("show unwatched video titles of 'Learning Kotlin' and 'Unlearning Java' on page") {
         VideoService.setFetchURLToJsonFunction { url ->
             Json.encodeToString(
-            listOf(
-                Video(1, "Learning Kotlin"),
-                Video(2, "Unlearning Java")
-            ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
+                listOf(
+                    Video(1, "Learning Kotlin"), Video(2, "Unlearning Java")
+                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
         }
         val actualVideoTitleList = VideoService.getVideos().map { it.title }
         withClue("unwatched video titles") {
@@ -23,38 +22,29 @@ class VideoServiceTest: ShouldSpec( {
         }
     }
 
-//            should("show unwatched video titles of 'Learning Kotlin' on page") {
-//                VideoService.setFetchURLToJsonFunction { url ->
-//                    Json.encodeToString(
-//                        listOf(
-//                            Video(1, "Learning Kotlin")
-//                        ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
-//                }
-//                ForComponent(AppComponent) {
-//                    val actualUnwatchedVideoTitlesList = container.querySelectorAll("[data-code-element-handle='unwatchedVideo']")
-//                        .asList()
-//                        .map { it.textContent }
-//                    withClue("unwatched video titles") {
-//                        actualUnwatchedVideoTitlesList shouldContainExactly listOf("Learning Kotlin")
-//                    }
-//                }
-//            }
-//
-//            should("show unwatched video titles of 'Unlearning Java' on page 2") {
-//                VideoService.setFetchURLToJsonFunction { url ->
-//                    Json.encodeToString(
-//                        listOf(
-//                            Video(2, "Unlearning Java")
-//                        ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
-//                }
-//                ForComponent(AppComponent) {
-//                    val actualUnwatchedVideoTitlesList = container.querySelectorAll("[data-code-element-handle='unwatchedVideo']")
-//                        .asList()
-//                        .map { it.textContent }
-//                    withClue("unwatched video titles") {
-//                        actualUnwatchedVideoTitlesList shouldContainExactly listOf("Unlearning Java")
-//                    }
-//                }
-//            }
-}
-)
+    should("show unwatched video titles of 'Learning Kotlin' on page") {
+        VideoService.setFetchURLToJsonFunction { url ->
+            Json.encodeToString(
+                listOf(
+                    Video(1, "Learning Kotlin")
+                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
+        }
+        val actualUnwatchedVideoTitlesList = VideoService.getVideos().map { it.title }
+        withClue("unwatched video titles") {
+            actualUnwatchedVideoTitlesList shouldContainExactly listOf("Learning Kotlin")
+        }
+    }
+
+    should("show unwatched video titles of 'Unlearning Java' on page 2") {
+        VideoService.setFetchURLToJsonFunction { url ->
+            Json.encodeToString(
+                listOf(
+                    Video(2, "Unlearning Java")
+                ).firstOrNull { it.id == url.substringAfterLast('/').toIntOrNull() })
+        }
+        val actualUnwatchedVideoTitlesList = VideoService.getVideos().map { it.title }
+        withClue("unwatched video titles") {
+            actualUnwatchedVideoTitlesList shouldContainExactly listOf("Unlearning Java")
+        }
+    }
+})
