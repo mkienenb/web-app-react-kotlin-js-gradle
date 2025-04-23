57f2b10f6fe120ca5ed32e0f4fe44be5efc3b950
jzdewilde <jzdewilde@gvea.com>
2025-04-23 10:49:28 -0800

    test: add "fetch video title Responses of 'Learning Kotlin'"

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -3,9 +3,11 @@ package api
 import confexplorer.viewvideo.Video
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import org.w3c.fetch.Response
 
 object VideoService {
     private var fetchURLToJsonFunction : (String) -> String? = {null}
+    private var fetchURLToResponseFunction : (String) -> Response? = {null}
 
     fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
@@ -13,6 +15,10 @@ object VideoService {
 
     fun setFetchURLToJsonFunction(fetchURLToJsonFunction: (String) -> String?) {
         this.fetchURLToJsonFunction = fetchURLToJsonFunction
+    }
+
+    fun setFetchURLToResponseFunction(fetchURLToResponseFunction: (String) -> Response?) {
+        this.fetchURLToResponseFunction = fetchURLToResponseFunction
     }
 
     private fun getVideo(videoId: Int): Video? {
