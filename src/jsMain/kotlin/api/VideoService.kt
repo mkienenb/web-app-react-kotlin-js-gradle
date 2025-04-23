d265be1601f7501c82e63307e57cc85eb4b306e1
RootCellar <RootCellar9877@gmail.com>
2025-04-23 11:11:04 -0800

    test: switch to Promise<Response> fetch function

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.await
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.w3c.fetch.Response
+import kotlin.js.Promise
 
 object VideoService {
     private var fetchURLToJsonFunction : (String) -> String? = {null}
     private lateinit var fetchURLToResponseFunction : (String) -> Response
+    private lateinit var fetchURLToPromiseResponseFunction : (String) -> Promise<Response>
 
     suspend fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
@@ -18,8 +20,8 @@ object VideoService {
         this.fetchURLToJsonFunction = fetchURLToJsonFunction
     }
 
-    fun setFetchURLToResponseFunction(fetchURLToResponseFunction: (String) -> Response) {
-        this.fetchURLToResponseFunction = fetchURLToResponseFunction
+    fun setFetchURLToPromiseResponseFunction(fetchURLToPromiseResponseFunction: (String) -> Promise<Response>) {
+        this.fetchURLToPromiseResponseFunction = fetchURLToPromiseResponseFunction
     }
 
     private suspend fun getVideo(videoId: Int): Video? {
