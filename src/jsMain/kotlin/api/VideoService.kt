55f493a1a9ab28b489b3f987d9d69ec5a3469604
Mike Kienenberger <mkienenb@gmail.com>
2025-04-23 13:37:43 -0400

    feat: update getVideo to use fetchURLToJsonFunction()

--- src/jsMain/kotlin/api/VideoService.kt
+++ src/jsMain/kotlin/api/VideoService.kt
@@ -1,17 +1,14 @@
 package api
 
 import confexplorer.viewvideo.Video
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 object VideoService {
-    private var fetchURLFunction : (String) -> Video? = {null}
     private var fetchURLToJsonFunction : (String) -> String? = {null}
 
     fun getVideos(): List<Video> {
         return (1..2).mapNotNull{ getVideo(it) }
-    }
-
-    fun setFetchURLFunction(fetchURLFunction: (String) -> Video?) {
-        this.fetchURLFunction = fetchURLFunction
     }
 
     fun setFetchURLToJsonFunction(fetchURLToJsonFunction: (String) -> String?) {
@@ -19,6 +16,17 @@ object VideoService {
     }
 
     fun getVideo(videoId: Int): Video? {
-        return fetchURLFunction("/$videoId")
+
+        // input url
+
+
+        // output json rep of a video
+        val json = fetchURLToJsonFunction("/$videoId")
+
+        return if (!json.isNullOrEmpty()) {
+            Json.decodeFromString<Video>(json)
+        } else {
+            null
+        }
     }
 }
