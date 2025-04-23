ac9e4d962c1cac5b425ee251ca4b129c9135ec0c
Mike Kienenberger <mkienenb@gmail.com>
2025-04-23 18:26:09 -0400

    test: fix FakeWebService url-matching

--- src/jvmTest/kotlin/cucumber/common/fakewebservice/FakeWebService.kt
+++ src/jvmTest/kotlin/cucumber/common/fakewebservice/FakeWebService.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
@@ -45,14 +46,15 @@ class FakeWebService {
                 allowHeader(HttpHeaders.ContentType)
             }
             routing {
-                get("/{key}") {
-                    val requestedPath = call.parameters["key"]!!
-                    println("FakeWebService for port $port received request for [$requestedPath]")
-                    val responseText = getResponse(requestedPath)
+                get("{...}") {
+                    val key = call.request.uri
+                    println("FakeWebService for port $port received request for [$key]")
+                    val responseText = getResponse(key)
                     println("FakeWebService for port $port responded with [$responseText]")
-                    call.respondText(responseText ?: "Not found", contentType = ContentType.Application.Json.withCharset(Charsets.UTF_8))
-//                    call.respondText(responseText ?: "Not found", contentType = ContentType.Application.Json)
-//                    call.respondText(responseText ?: "Not found")
+                    call.respondText(
+                        responseText ?: "Not found",
+                        contentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
+                    )
                 }
             }
         }
