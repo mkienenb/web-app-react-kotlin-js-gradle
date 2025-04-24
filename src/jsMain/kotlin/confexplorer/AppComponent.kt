0dea8a8983dfcec350fc17fa8cb2c2e13a362570
jzdewilde <jzdewilde@gvea.com>
2025-04-24 10:19:56 -0800

    feat: fetch videos from VideoService

--- src/jsMain/kotlin/confexplorer/AppComponent.kt
+++ src/jsMain/kotlin/confexplorer/AppComponent.kt
@@ -1,21 +1,32 @@
 package confexplorer
 
 import api.VideoService
+import confexplorer.viewvideo.Video
 import confexplorer.viewvideo.VideoListReactComponent
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.p
+import react.useEffectOnce
+import react.useState
 import testsupport.dataCodeElementHandleAttribute
 
 
 val AppComponent = FC<Props> {
+    var videoList: List<Video> by useState(emptyList())
+
+    useEffectOnce {
+        MainScope().launch {
+            videoList = VideoService.getVideos()
+        }
+    }
     h1 {
         +"Conference Explorer"
     }
 
     VideoListReactComponent {
-//        videos = VideoService.getVideos()
-        videos = TODO()
+        videos = videoList
     }
 }
