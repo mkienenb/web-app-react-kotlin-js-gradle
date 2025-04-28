2352fbf254454456c96d13a5f46a2cca23cc1fdc
jzdewilde <jzdewilde@gvea.com>
2025-04-28 10:10:01 -0800

    test: use waitUntilDoneLoading() instead of Thead.sleep

--- src/jvmTest/kotlin/cucumber/confexplorer/ViewVideoPage.kt
+++ src/jvmTest/kotlin/cucumber/confexplorer/ViewVideoPage.kt
@@ -5,8 +5,12 @@ import cucumber.common.page.GenerateCucumberPageHelper
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
-import org.openqa.selenium.support.FindBy
 import org.openqa.selenium.support.PageFactory
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.toJavaDuration
 
 @GenerateCucumberPageHelper
 class ViewVideoPage(driver: WebDriver) : BasePage(driver) {
@@ -30,5 +34,15 @@ class ViewVideoPage(driver: WebDriver) : BasePage(driver) {
             }
         }
         throw RuntimeException("Video not found")
+    }
+
+    fun waitUntilDoneLoading(
+        timeout: Duration = 5000.milliseconds
+    ) {
+        val wait = WebDriverWait(driver, timeout.toJavaDuration())
+        wait.until(
+            ExpectedConditions.invisibilityOfElementLocated
+                (By.cssSelector("[data-code-element-handle='loading']"))
+        )
     }
 }
