a0a13dca45d9837b39cce10269c70046ff912000
Mike Kienenberger <mkienenb@gmail.com>
2025-04-29 15:33:23 -0400

    test: change should("show Learning kotlin video") to locate name by data-code-element-handle

--- src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
+++ src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
@@ -14,7 +14,7 @@ class VideoListTest : ReactComponentTestBase() {
                 ForComponent(VideoList, {
                     videos = listOf(Video(1, "Learning kotlin"))
                 }) {
-                    val firstVideoTitle = container.querySelector("ul li")?.textContent
+                    val firstVideoTitle = container.querySelector("[data-code-element-handle='unwatchedVideo']")?.textContent
                     withClue("unordered list") {
                         firstVideoTitle shouldBe "Learning kotlin"
                     }
