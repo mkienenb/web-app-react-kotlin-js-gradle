0085d04f7553ffcf070dd2a13c82531677063c93
Mike Kienenberger <mkienenb@gmail.com>
2025-04-30 16:04:37 -0400

    test: wip to test react components with testModule for dependencies

--- src/jsTest/kotlin/confexplorer/AppTest.kt
+++ src/jsTest/kotlin/confexplorer/AppTest.kt
@@ -9,6 +9,14 @@ import io.kotest.matchers.shouldBe
 import js.array.asList
 import kotest.ReactComponentTestBase
 import kotlinx.coroutines.CompletableDeferred
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.instance
+import org.kodein.di.singleton
+import react.act
+import react.create
+import react.dom.client.createRoot
+import reactdi.KodeinProvider
 
 class AppTest: ReactComponentTestBase() {
     init {
@@ -42,25 +50,36 @@ class AppTest: ReactComponentTestBase() {
             }
 
             should("show 'Loading...' when video list is still loading") {
-                Env.testServiceVideoUrl = "http://localhost"
                 val videoList = listOf(
                     Video(1, "Learning Kotlin"),
                     Video(2, "Unlearning Java")
                 )
                 val fetchGate = CompletableDeferred<Unit>()
-                val controlledFetchFunction : URLToPromiseResponseFunction = { url ->
-                    fetchGate.await()
-                    createPromiseResponseFetchFunction(videoList)(url)
+                val testModule = DI.Module("test") {
+                    bind<URLToPromiseResponseFunction>() with singleton {
+                        { url ->
+                            fetchGate.await()
+                            createPromiseResponseFetchFunction(videoList)(url)
+                        }
+                    }
+                    bind<UrlProvider>() with singleton {
+                        object : UrlProvider {
+                            override fun getBaseUrl(): String = "http://localhost"
+                        }
+                    }
+
+                    bind<VideoService> {singleton { VideoService(instance(), instance()) }}
                 }
 
-                //VideoService.setFetchURLToPromiseResponseFunction(controlledFetchFunction)
-                ForComponentCallingCoroutines(App) {
-                    val actualVideoListsElement = container.querySelector("[data-code-element-handle='videoLists']")
-                    withClue("video lists element") {
-                        actualVideoListsElement?.textContent shouldBe "Loading..."
-                    }
-                    fetchGate.complete(Unit)
+                Env.testServiceVideoUrl = "http://localhost"
+
+
+                actRenderComponent(App, {})  // now we use our extracted act-render helper
+                val actualVideoListsElement = container.querySelector("[data-code-element-handle='videoLists']")
+                withClue("video lists element") {
+                    actualVideoListsElement?.textContent shouldBe "Loading..."
                 }
+                fetchGate.complete(Unit)  // then run the suspendable test code
             }
         }
     }
