e21155a1c1bbed0757263fd13fa51e45158038bd
jzdewilde <jzdewilde@gvea.com>
2025-05-01 10:45:30 -0800

    test: switch to ReactShouldSpecBase()

--- src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
+++ src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
@@ -1,7 +1,9 @@
 package confexplorer.viewvideo
 
 import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
+import js.array.asList
 import kotest.suspendSetup
 import reactdi.ReactShouldSpecBase
 
@@ -21,33 +23,43 @@ class VideoListTest : ReactShouldSpecBase() {
                 }
             }()
         }
-//            should("show Unlearning Java video") {
-//                ForComponent(VideoList, {
-//                    videos = listOf(Video(1, "Unlearning Java"))
-//                }) {
-//                    val firstVideoTitle = container.querySelector("ul li")?.textContent
-//                    withClue("unordered list") {
-//                        firstVideoTitle shouldBe "Unlearning Java"
-//                    }
-//                }
-//            }
-//            should("show Learning kotlin and Unlearning Java videos") {
-//                ForComponent(VideoList, {
-//                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Unlearning Java"))
-//                }) {
-//                    val actualVideoTitles = container.querySelectorAll("ul li").asList().map { it.textContent }
-//                    withClue("unordered list") {
-//                        actualVideoTitles shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
-//                    }
-//                }
-//            }
-//
-//            xshould("show video selection symbol when 'Learning react' video is selected") {
-//                ForComponent(VideoList, {
-//                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Learning React"))
-//                }) {
-//                    TODO()
-//                }
-//            }
+
+        should("show Unlearning Java video") {
+            suspendSetup(object {
+                val videoList = listOf(Video(1, "Unlearning Java"))
+            }).exercise {
+                renderReactComponent(VideoList) {
+                    videos = videoList
+                }
+                container.querySelector("[data-code-element-handle='unwatched-video-title']")?.textContent
+            }.verify {firstVideoTitle: String? ->
+                withClue("unordered list") {
+                    firstVideoTitle shouldBe "Unlearning Java"
+                }
+            }()
+        }
+
+        should("show Learning kotlin and Unlearning Java videos") {
+            suspendSetup(object {
+                val videoList = listOf(Video(1, "Learning Kotlin"), Video(2, "Unlearning Java"))
+            }).exercise {
+                renderReactComponent(VideoList) {
+                    videos = videoList
+                }
+                container.querySelectorAll("[data-code-element-handle='unwatched-video-title']").asList().map { it.textContent }
+            }.verify {actualVideoTitles: List<String>? ->
+                withClue("unordered list") {
+                    actualVideoTitles shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
+                }
+            }()
+        }
+
+        xshould("show video selection symbol when 'Learning react' video is selected") {
+            suspendSetup(object {
+                val videoList = listOf(Video(1, "Learning Kotlin"), Video(2, "Learning react"))
+            }).exercise {
+                TODO()
+            }
+        }
     }
 }
