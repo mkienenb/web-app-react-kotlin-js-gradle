d25b70089c9cceddb924bf967a72994990a70ad9
RootCellar <RootCellar9877@gmail.com>
2025-05-01 10:34:50 -0800

    test: use new test framework

--- src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
+++ src/jsTest/kotlin/confexplorer/viewvideo/VideoListTest.kt
@@ -1,53 +1,53 @@
 package confexplorer.viewvideo
 
-import browserOnlyCode
 import io.kotest.assertions.withClue
-import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
-import js.array.asList
-import kotest.ReactComponentTestBase
+import kotest.suspendSetup
+import reactdi.ReactShouldSpecBase
 
-class VideoListTest : ReactComponentTestBase() {
+class VideoListTest : ReactShouldSpecBase() {
     init {
-        browserOnlyCode {
-            should("show Learning kotlin video") {
-                ForComponent(VideoList, {
-                    videos = listOf(Video(1, "Learning kotlin"))
-                }) {
-                    val firstVideoTitle = container.querySelector("[data-code-element-handle='unwatched-video-title']")?.textContent
-                    withClue("unordered list") {
-                        firstVideoTitle shouldBe "Learning kotlin"
-                    }
+        should("show Learning kotlin video") {
+            suspendSetup(object {
+                val videoList = listOf(Video(1, "Learning kotlin"))
+            }).exercise {
+                renderReactComponent(VideoList) {
+                    videos = videoList
                 }
-            }
-            should("show Unlearning Java video") {
-                ForComponent(VideoList, {
-                    videos = listOf(Video(1, "Unlearning Java"))
-                }) {
-                    val firstVideoTitle = container.querySelector("ul li")?.textContent
-                    withClue("unordered list") {
-                        firstVideoTitle shouldBe "Unlearning Java"
-                    }
+                container.querySelector("[data-code-element-handle='unwatched-video-title']")?.textContent
+            }.verify {firstVideoTitle: String? ->
+                withClue("unordered list") {
+                    firstVideoTitle shouldBe "Learning kotlin"
                 }
-            }
-            should("show Learning kotlin and Unlearning Java videos") {
-                ForComponent(VideoList, {
-                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Unlearning Java"))
-                }) {
-                    val actualVideoTitles = container.querySelectorAll("ul li").asList().map { it.textContent }
-                    withClue("unordered list") {
-                        actualVideoTitles shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
-                    }
-                }
-            }
-
-            xshould("show video selection symbol when 'Learning react' video is selected") {
-                ForComponent(VideoList, {
-                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Learning React"))
-                }) {
-                    TODO()
-                }
-            }
+            }()
         }
+//            should("show Unlearning Java video") {
+//                ForComponent(VideoList, {
+//                    videos = listOf(Video(1, "Unlearning Java"))
+//                }) {
+//                    val firstVideoTitle = container.querySelector("ul li")?.textContent
+//                    withClue("unordered list") {
+//                        firstVideoTitle shouldBe "Unlearning Java"
+//                    }
+//                }
+//            }
+//            should("show Learning kotlin and Unlearning Java videos") {
+//                ForComponent(VideoList, {
+//                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Unlearning Java"))
+//                }) {
+//                    val actualVideoTitles = container.querySelectorAll("ul li").asList().map { it.textContent }
+//                    withClue("unordered list") {
+//                        actualVideoTitles shouldContainExactly listOf("Learning Kotlin", "Unlearning Java")
+//                    }
+//                }
+//            }
+//
+//            xshould("show video selection symbol when 'Learning react' video is selected") {
+//                ForComponent(VideoList, {
+//                    videos = listOf(Video(1, "Learning Kotlin"), Video(2, "Learning React"))
+//                }) {
+//                    TODO()
+//                }
+//            }
     }
 }
