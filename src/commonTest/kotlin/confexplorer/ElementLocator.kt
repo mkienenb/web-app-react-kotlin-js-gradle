9a0bcdbf6cb718b7fe8a2a0355a4ea8688ce4bc8
tmv <tmv@gvea.com>
2025-05-02 10:32:22 -0800

    test: find unwatched video by role=option instead of li element

--- src/commonTest/kotlin/confexplorer/ElementLocator.kt
+++ src/commonTest/kotlin/confexplorer/ElementLocator.kt
@@ -2,5 +2,5 @@ package confexplorer
 
 object ElementLocator {
     const val UNWATCHED_VIDEO_TITLE_FOR_SELECTED_VIDEO_XPATH_EXPRESSION = """
-                    //li[@aria-selected='true']"""
+                    //*[@role='option' and @aria-selected='true']"""
 }
