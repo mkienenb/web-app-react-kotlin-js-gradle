e6c0e2efe550d3303a8ad14d63478b6e20f40b3d
lavalo98 <lavaloprogramming@gmail.com>
2025-05-02 11:43:28 -0800

    feat: change selected video onClick

--- src/jsMain/kotlin/confexplorer/viewvideo/VideoList.kt
+++ src/jsMain/kotlin/confexplorer/viewvideo/VideoList.kt
@@ -1,32 +1,29 @@
 package confexplorer.viewvideo
 
-import confexplorer.UISymbol.VIDEO_SELECTOR_SYMBOL
 import react.FC
 import react.Props
 import react.dom.aria.AriaRole
 import react.dom.html.ReactHTML.li
-import react.dom.html.ReactHTML.span
 import react.dom.html.ReactHTML.ul
 import testsupport.dataCodeElementHandleAttribute
 
 external interface VideoListProps : Props {
     var videos: List<Video>
+    var selectedVideo : Video?
+    var onSelectVideo: (Video) -> Unit
 }
 
 val VideoList = FC<VideoListProps> { props ->
     ul {
-        props.videos.forEach {
+        props.videos.forEach { video ->
             li {
                 role= AriaRole.option
-                ariaSelected = it.title == "Learning kotlin"
+                ariaSelected = video == props.selectedVideo
+                onClick = {
+                    props.onSelectVideo(video)
+                }
                 dataCodeElementHandleAttribute = "unwatched-video-title"
-                +it.title
-//                if (it.title == "Learning kotlin") {
-//                    span {
-//                        dataCodeElementHandleAttribute = "video-selection-indicator"
-//                        +VIDEO_SELECTOR_SYMBOL
-//                    }
-//                }
+                +video.title
             }
         }
     }
