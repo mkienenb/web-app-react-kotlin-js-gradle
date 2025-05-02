f4ec6ed2dddf24bef2d47d1844b9bb27aee3332f
RootCellar <RootCellar9877@gmail.com>
2025-05-02 10:07:10 -0800

    test: fix element locator to find selected video correctly

--- src/commonTest/kotlin/confexplorer/ElementLocator.kt
+++ src/commonTest/kotlin/confexplorer/ElementLocator.kt
@@ -2,6 +2,5 @@ package confexplorer
 
 object ElementLocator {
     const val UNWATCHED_VIDEO_TITLE_FOR_SELECTED_VIDEO_XPATH_EXPRESSION = """
-                    //span[@data-code-element-handle='video-selection-indicator']
-                    /following-sibling::span[@data-code-element-handle='unwatched-video-title']"""
+                    //li[@aria-selected='true']"""
 }
