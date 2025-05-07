e4764aee540060bf3dfcaf41c4331a2ca8b1d325
RootCellar <RootCellar9877@gmail.com>
2025-05-07 10:57:32 -0800

    refactor: allow ReactPlayer to be replaced

--- src/jsTest/kotlin/confexplorer/ConfExplorerTestBase.kt
+++ src/jsTest/kotlin/confexplorer/ConfExplorerTestBase.kt
@@ -1,8 +1,15 @@
 package confexplorer
 
-import confexplorer.viewvideo.ReactPlayerMock
+import confexplorer.viewvideo.ReactPlayerProps
 import confexplorer.viewvideo.reactPlayer
+import react.FC
+import react.dom.html.ReactHTML
 import reactdi.ReactShouldSpecBase
+
+val ReactPlayerMock = FC<ReactPlayerProps> { props ->
+    ReactHTML.div {
+    }
+}
 
 open class ConfExplorerTestBase: ReactShouldSpecBase() {
     init {
