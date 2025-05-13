1ddb7889c929bb9fe4fe5df35ece9701eb2beeba
RootCellar <RootCellar9877@gmail.com>
2025-05-13 12:07:48 -0800

    feat: add a mark as watched button

--- src/jsMain/kotlin/confexplorer/viewvideo/VideoPlayer.kt
+++ src/jsMain/kotlin/confexplorer/viewvideo/VideoPlayer.kt
@@ -1,10 +1,12 @@
 package confexplorer.viewvideo
 
+import confexplorer.ElementHandle.MARK_AS_WATCHED_BUTTON
 import confexplorer.ElementHandle.REACT_PLAYER
 import confexplorer.ElementHandle.VIDEO_DETAIL_TITLE
 import react.FC
 import react.Props
 import react.dom.aria.AriaRole
+import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h3
 import react.dom.html.ReactHTML.span
@@ -19,6 +21,9 @@ val VideoPlayer = FC<VideoPlayerProps> { props ->
         h3 {
             ariaLabel = VIDEO_DETAIL_TITLE
             +props.video.title
+        }
+        button {
+            ariaLabel = MARK_AS_WATCHED_BUTTON
         }
         span {
             ariaLabel = REACT_PLAYER
