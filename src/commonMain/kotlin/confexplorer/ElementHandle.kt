230365daa2145c20a05b7a6fc23776673721c498
lavalo98 <lavaloprogramming@gmail.com>
2025-05-13 11:27:03 -0800

    refactor: update constants for screen reader compat

--- src/commonMain/kotlin/confexplorer/ElementHandle.kt
+++ src/commonMain/kotlin/confexplorer/ElementHandle.kt
@@ -1,9 +1,9 @@
 package confexplorer
 
 object ElementHandle {
-    const val UNWATCHED_VIDEO_TITLE = "unwatched-video-title"
-    const val VIDEO_DETAIL_TITLE = "video-detail-title"
-    const val REACT_PLAYER = "react-player"
+    const val UNWATCHED_VIDEO_TITLE = "unwatched video title"
+    const val VIDEO_DETAIL_TITLE = "video detail title"
+    const val REACT_PLAYER = "video player"
     const val LOADING = "loading"
-    const val UNWATCHED_VIDEO_LIST = "unwatched-video-list"
+    const val UNWATCHED_VIDEO_LIST = "unwatched video list"
 }
