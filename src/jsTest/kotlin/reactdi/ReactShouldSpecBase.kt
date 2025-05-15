6180e12b5caa9631cb58aea65a89612d2898874a
RootCellar <RootCellar9877@gmail.com>
2025-05-15 09:46:59 -0800

    test: support testing react components using react testing library under jsNodeTest

---
+++ src/jsTest/kotlin/reactdi/ReactShouldSpecBase.kt
@@ -0,0 +1,42 @@
+package reactdi
+
+import io.kotest.core.spec.style.ShouldSpec
+import kotest.testDI
+import react.FC
+import react.Props
+import com.zegreatrob.wrapper.testinglibrary.react.TestingLibraryReact.act
+import react.create
+import react.dom.client.createRoot
+import web.dom.document
+import web.html.HTMLDivElement
+
+open class ReactShouldSpecBase : ShouldSpec() {
+
+    protected lateinit var container: HTMLDivElement
+
+    init {
+        beforeTest {
+            container = document.createElement("div") as HTMLDivElement
+            document.body.appendChild(container)
+        }
+
+        afterTest {
+            container.remove()
+        }
+    }
+
+    protected suspend fun <P : Props> renderReactComponent(componentUnderTest: FC<P>,
+                                               propsBuilder: P.() -> Unit = {}) {
+        val root = createRoot(container)
+        act {
+            root.render(KodeinProvider.create {
+                di = testDI
+                +componentUnderTest.create(propsBuilder)
+            })
+        }
+    }
+
+    protected fun runningInBrowser(): Boolean {
+        return js("typeof window.fetch !== 'undefined'") as Boolean
+    }
+}
